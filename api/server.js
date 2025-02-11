57a03c8f4e8608828283afaf7d78a2e062582e07
N.I <iworksWeb@gmail.com>
2025-02-11 22:53:43 +0900

    api endpoint /api/auth/login

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const { PrismaClient } = require("@prisma/client");
+const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 
+dotenv.config();
 const PORT = 5000;
 const app = express();
 const prisma = new PrismaClient();
@@ -31,4 +34,34 @@ app.post("/api/auth/register", async (req, res) => {
   });
 
   return res.json({ user });
+});
+
+//**
+// POST: /api/auth/login
+// {
+//   "email": "teijisdjagi@co.jp",
+//   "password": "hashedPassword"
+// }
+// response
+// {
+//   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTczOTI4MTg0NCwiZXhwIjoxNzM5Mjg1NDQ0fQ.4JQw2PXTayxn1UQHngqJtY9x8jaP2Fb09IR1Kul7gAg"
+// }
+//  */
+app.post("/api/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) return res.status(401).json({ error: "Invalid credentials" });
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid)
+    return res.status(401).json({ error: "Invalid credentials" });
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  return res.json({ token });
 });
