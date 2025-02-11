5cefcda7b48d5f11bd805a91d0c814baa2e82033
N.I <iworksWeb@gmail.com>
2025-02-11 19:58:54 +0900

    api endpoint api/auth/register:POST

--- api/server.js
+++ api/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
 const PORT = 5000;
 const app = express();
+const prisma = new PrismaClient();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.post("api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+
+  return res.json({ user });
 });
