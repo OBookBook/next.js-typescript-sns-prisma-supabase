8ad30c9c8fbc736012a4ad8bf5d14796b1dbb3e3
N.I <iworksWeb@gmail.com>
2025-02-11 23:20:13 +0900

    client add app/components/Timeline.tsx

---
+++ client/app/components/Timeline.tsx
@@ -0,0 +1,24 @@
+const Timeline = () => {
+  return (
+    <div className="min-h-screen bg-zinc-50">
+      <main className="container mx-auto py-8 px-4">
+        <div className="bg-white shadow-xl rounded-xl p-6 mb-4 border border-zinc-100">
+          <form>
+            <textarea
+              className="w-full h-32 p-4 border border-zinc-200 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:border-transparent transition-all  text-zinc-800 placeholder-zinc-400"
+              placeholder="What's on your mind?"
+            ></textarea>
+            <button
+              type="submit"
+              className="mt-4 bg-zinc-900 hover:bg-zinc-800 duration-300 text-white font-medium py-3 px-6 rounded-lg shadow-md hover:shadow-xl transform hover:-translate-y-0.5 transition-all"
+            >
+              投稿
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Timeline;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,9 @@
+import Timeline from "./components/Timeline";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center">Hello World</h1>
+      <Timeline />
     </div>
   );
 }
