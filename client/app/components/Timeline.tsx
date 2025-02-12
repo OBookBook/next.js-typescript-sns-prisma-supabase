2d7bd87513448c8ef9b4bd27606f4625697f3222
N.I <iworksWeb@gmail.com>
2025-02-13 07:54:23 +0900

    api feat endpoint /api/posts/post

--- client/app/components/Timeline.tsx
+++ client/app/components/Timeline.tsx
@@ -1,12 +1,16 @@
+"use server";
+
 import Post from "./Post";
+import { handlePost } from "../lib/actions";
 
 const Timeline = () => {
   return (
     <div className="min-h-screen bg-zinc-50">
       <main className="container mx-auto py-8 px-4">
         <div className="bg-white shadow-xl rounded-xl p-6 mb-4 border border-zinc-100">
-          <form>
+          <form action={handlePost}>
             <textarea
+              name="content"
               className="w-full h-32 p-4 border border-zinc-200 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:border-transparent transition-all  text-zinc-800 placeholder-zinc-400"
               placeholder="What's on your mind?"
             ></textarea>
---
+++ client/app/lib/actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { z } from "zod";
+
+const schema = z.object({
+  content: z.string().min(1, "内容を入力してください"),
+});
+
+export async function handlePost(formData: FormData) {
+  const rawFormData = {
+    content: formData.get("content"),
+  };
+
+  try {
+    const validatedFields = schema.parse(rawFormData);
+    const response = await fetch("http://localhost:5000/api/posts/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: validatedFields.content,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || "Post failed");
+    }
+  } catch (error: any) {
+    console.log(error);
+  }
+}
